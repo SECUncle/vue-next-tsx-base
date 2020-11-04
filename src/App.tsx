f5afee520eb2a46fd276ea196a699327ea949194
wangyaju <wangyaju@didiglobal.com>
2020-11-04 19:27:48 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ export default defineComponent({
     return () => (
       <div>
         <div id="nav">
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-          <router-link to="/test">test</router-link>
-          <router-link to="/input">input</router-link>
-          <router-link to="/select">select</router-link>
-          <router-link to="/form">form</router-link>
-          <router-link to="/table">table</router-link>
+          <div class="article_test">
+            vue-next test
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/button">button</router-link>
+          </div>
+          <div class="article_componment">
+            componment
+            <router-link to="/input">input</router-link>
+            <router-link to="/select">select</router-link>
+            <router-link to="/icon">icon</router-link>
+            <router-link to="/tag">tag</router-link>
+            <router-link to="/popper">popper</router-link>
+            <router-link to="/form">form</router-link>
+            <router-link to="/table">table</router-link>
+          </div>
         </div>
         <router-view id="content"></router-view>
       </div>
